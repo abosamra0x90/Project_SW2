9d7bbb7aa8c85c8dbe539d75be54d81dea6c01fd
amir891 <63619894+amir891@users.noreply.github.com>
2022-05-17 21:07:35 +0200

    Update Arguments.java

--- supermarket/src/main/java/com/mycompany/supermarket/Arguments.java
+++ supermarket/src/main/java/com/mycompany/supermarket/Arguments.java
@@ -9,7 +9,9 @@ package com.mycompany.supermarket;
  * @author moham
  */
 public class Arguments {
-   
+    public int Product_Quantity,Quantity;
+    public float Product_Price,No_Gain_Price,TotalPrice;
+    public String Product_Name,IProduct_ID,Product_Expiration,Product_Production,Product_Catcegories;
     public javax.swing.JPasswordField newpassword,password,renewpassword;
     public javax.swing.JTextField username,newusername;
     
