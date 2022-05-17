2bf3c870cf0ae0ec528a8d443a6a190d88edb609
Mohamed1Tarek <104506806+Mohamed1Tarek@users.noreply.github.com>
2022-05-17 22:14:20 +0200

    Add files via upload
    Supplier class + SupplierGui

---
+++ supermarket/src/main/java/com/mycompany/supermarket/Supplier.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.supermarket;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.mycompany.supermarket.database.DatabaseOperations;
+import javax.swing.JTable;
+/**
+ *
+ * @author moham
+ */
+public class Supplier implements DatabaseOperations {
+    private String Name , Telphone;
+    public Supplier(String Name,String Telphone){
+        this.Name = Name;
+        this.Telphone = Telphone;
+    }
+    @Override
+    public void Insert() {
+        try {
+            PreparedStatement preparedStatement = com.mycompany.supermarket.database.DatabasePraparedQuery.Database_PreparedStatement(com.mycompany.supermarket.database.SQLQueries.AddSupplier());
+            preparedStatement.setString(1,this.Telphone);
+            preparedStatement.setString(2,this.Name);
+            
+            ResultSet Resulte = preparedStatement.executeQuery();
+            if(Resulte.next()){
+                GoodResult.InsertDone();
+            } 
+        } 
+        catch (SQLException ex) {
+            Logger.getLogger(Supplier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void Update() {
+        try {
+            PreparedStatement preparedStatement = com.mycompany.supermarket.database.DatabasePraparedQuery.Database_PreparedStatement(com.mycompany.supermarket.database.SQLQueries.UpdateSupplier());
+            preparedStatement.setString(1,this.Telphone);
+            preparedStatement.setString(2,this.Name);
+            ResultSet Resulte = preparedStatement.executeQuery();
+            if(Resulte.next()){
+                GoodResult.IUpdateDone();
+            }
+        } 
+        catch (SQLException ex) {
+            Logger.getLogger(Supplier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void Delete() {
+        try {
+            PreparedStatement preparedStatement = com.mycompany.supermarket.database.DatabasePraparedQuery.Database_PreparedStatement(com.mycompany.supermarket.database.SQLQueries.DeleteSupplier());
+            preparedStatement.setString(1,this.Name);
+            ResultSet Resulte = preparedStatement.executeQuery();
+            if(Resulte.next()){
+                GoodResult.DeleteDone();
+            }
+        } 
+        catch (SQLException ex) {
+            Logger.getLogger(Supplier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public static void Show_All_Supplier(JTable jtable){
+        ResultSet Resulte = GetDataFromDataBase.GetTableSupplier();
+        DataInViwe ShowSuppliers = new DataInViwe();
+        try {
+            ShowSuppliers.ShowTableSupplier(Resulte, jtable);
+        } catch (SQLException ex) {
+            Logger.getLogger(Supplier.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
